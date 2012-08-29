8e71e5e270ab249915874aa594a61d4980a59029
James Lammlein <j.lammlein@gmail.com>
2012-08-28 20:08:18 -0400

    Added a try catch in case opening the file fails.  Not sure what to do if it does though.

--- Logger/Logger.cs
+++ Logger/Logger.cs
@@ -1,4 +1,32 @@
-﻿using System;
+﻿
+
+/*
+LOLViewer
+Copyright 2011-2012 James Lammlein 
+
+ 
+
+This file is part of LOLViewer.
+
+LOLViewer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+LOLViewer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with LOLViewer.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,10 +61,15 @@ namespace CSharpLogger
 
         private void Flush()
         {
-            if (writer == null || writer.BaseStream == null)
-                writer = new StreamWriter(filePath, true);
-            writer.Write(builder);
-            builder.Clear();
+            try
+            {
+                if (writer == null || writer.BaseStream == null)
+                    writer = new StreamWriter(filePath, true);
+
+                writer.Write(builder);
+                builder.Clear();
+            }
+            catch { }
         }
 
         /// <summary>
